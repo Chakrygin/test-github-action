a6b783d07f33f456bb874279d0e739732d5cf914
Igor Chakrygin <igor@chakrygin.ru>
2022-02-19 20:29:24 +0300

    test

--- .github/actions/test/src/main.ts
+++ .github/actions/test/src/main.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 import { Telegraf } from 'telegraf'
+import * as Parser from 'rss-parser'
 
 const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 const TELEGRAM_TOKEN = core.getInput('TELEGRAM_TOKEN');
@@ -9,12 +10,22 @@ const TELEGRAM_TOKEN = core.getInput('TELEGRAM_TOKEN');
 async function main() {
   try {
 
-    const telegraf = new Telegraf(TELEGRAM_TOKEN, {
+    // const telegraf = new Telegraf(TELEGRAM_TOKEN, {
 
+    // });
+    // await telegraf.telegram.sendMessage(-1001767919878, 'Hello, World!', {
+
+    // })
+
+    const rss = new Parser();
+
+    let feed = await rss.parseURL('https://andrewlock.net/rss.xml')
+
+    console.log(feed.title);
+
+    feed.items.forEach(item => {
+      console.log(item.title + ':' + item.link)
     });
-    await telegraf.telegram.sendMessage(-1001767919878, 'Hello, World!', {
-
-    })
 
   } catch (error: any) {
     core.setFailed(error.message)
