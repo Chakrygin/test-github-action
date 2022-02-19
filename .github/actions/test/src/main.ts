398b65debd957b1d793b58749dc696f223aa2416
Igor Chakrygin <igor@chakrygin.ru>
2022-02-19 20:05:18 +0300

    test

--- .github/actions/test/src/main.ts
+++ .github/actions/test/src/main.ts
@@ -1,9 +1,18 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+import { Telegraf } from 'telegraf'
+
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const TELEGRAM_TOKEN = core.getInput('TELEGRAM_TOKEN');
 
 async function main() {
   try {
 
-    core.info(`Hello, World!`);
+    const telegraf = new Telegraf(TELEGRAM_TOKEN);
+    telegraf.telegram.sendMessage('chakrygin-sandbox', 'Hello, World!', {
+
+    })
 
   } catch (error: any) {
     core.setFailed(error.message)
