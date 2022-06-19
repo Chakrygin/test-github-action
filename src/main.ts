064c5bab773fe9443331cd0204de6706d79de38f
Igor Chakrygin <igor@chakrygin.ru>
2022-06-19 12:11:57 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ async function main() {
   try {
 
     // const response = await axios.get('https://radiodotnet.mave.digital/', {
-    const response = await axios.get('https://bookclub-dotnet.mave.digital/', {
+    // const response = await axios.get('https://bookclub-dotnet.mave.digital/', {
+    const response = await axios.get('https://radiodotnet.mave.digital/', {
       responseType: 'text',
       transformRequest: [],
       transformResponse: [],
+      maxContentLength: 1024 * 1024 * 1024,
     });
 
     console.log(response.data);
