83b230363f1d23df0460ecf00563cf1ced4eb037
Igor Chakrygin <igor@chakrygin.ru>
2023-01-11 23:34:37 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,24 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-// // import os from 'os';
+// import os from 'os';
 
-// async function main() {
-//   try {
+async function main() {
+  try {
 
-//     const t: any = {};
+    print('github.context', github.context);
 
-//     console.log(t.foo.bar);
+  }
+  catch (error: unknown) {
+    if (error instanceof Error) {
+      core.setFailed(error)
+    }
+  }
+}
 
-//   }
-//   catch (error: unknown) {
-//     if (error instanceof Error) {
-//       core.setFailed(error)
-//     }
-//   }
-// }
+function print(name: string, value: unknown) {
+  const json = JSON.stringify(value, null, 2);
+  core.info(name + ': ' + json + '\n');
+}
 
-// // function print(name: string, value: unknown) {
-// //   const json = JSON.stringify(value, null, 2);
-// //   core.info(name + ': ' + json + os.EOL);
-// // }
-
-// main();
-
-var x = { foo: 'test' } as any;
-
-core.info(x.test.bar);
+main();
