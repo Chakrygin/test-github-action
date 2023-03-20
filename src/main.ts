038d318598715aefa2308376c8e3e4265c23d980
Igor Chakrygin <igor@chakrygin.ru>
2023-03-20 20:31:40 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 import moment from 'moment';
-// import 'moment/locale/ru';
+import 'moment/locale/ru';
 
 async function main() {
   try {
 
-    const m1 = moment('13 февраля 2023', 'LL', 'ru');
-    const m2 = moment('2023-02-13').locale('ru');
+    moment.locale('en');
 
-    console.log('m1', m1);
-    console.log('m2', m2);
+    const m1 = moment('13 февраля 2023', 'LL', 'ru');
+    const m2 = moment('March 20, 2023', 'LL');
+    // const m2 = moment('2023-02-13').locale('ru');
+
+    console.log('m1', m1.format('LL'));
+    console.log('m2', m2.format('LL'));
 
   }
   catch (error) {
